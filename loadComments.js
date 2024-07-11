738dc9f6e139f0fe1e534f21e8c7729fc3062b86
Dpwerfew <157140578+Dpwerfew@users.noreply.github.com>
2024-07-11 14:39:13 +0300

    Update loadComments.js

--- loadComments.js
+++ loadComments.js
@@ -3,20 +3,37 @@ async function loadComments() {
     const commentsDiv = document.getElementById('comments');
     commentsDiv.innerHTML = '';
     const querySnapshot = await db.collection("comments").orderBy("timestamp", "desc").get();
+
+    const comments = {};
     querySnapshot.forEach((doc) => {
         const data = doc.data();
         const comment = document.createElement('div');
         comment.classList.add('comment');
-        if (data.parentId) {
-            comment.classList.add('reply');
-        }
+        comment.setAttribute('data-id', doc.id);
         comment.innerHTML = `<strong>${data.name}</strong> <em>${new Date(data.timestamp.seconds * 1000).toLocaleString()}</em><p>${data.text}</p>`;
         const replyButton = document.createElement('button');
         replyButton.innerText = "Ответить";
         replyButton.classList.add('reply-button');
         replyButton.onclick = () => showReplyForm(doc.id);
         comment.appendChild(replyButton);
-        commentsDiv.appendChild(comment);
+
+        comments[doc.id] = { element: comment, parentId: data.parentId };
+    });
+
+    // Build the comments tree
+    Object.keys(comments).forEach(id => {
+        const comment = comments[id];
+        if (comment.parentId) {
+            if (comments[comment.parentId]) {
+                const parentComment = comments[comment.parentId].element;
+                const repliesDiv = parentComment.querySelector('.replies') || document.createElement('div');
+                repliesDiv.classList.add('replies');
+                repliesDiv.appendChild(comment.element);
+                parentComment.appendChild(repliesDiv);
+            }
+        } else {
+            commentsDiv.appendChild(comment.element);
+        }
     });
 }
 
